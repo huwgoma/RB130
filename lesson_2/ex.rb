ae1c333150da3480a5aa374b7f88373ff6668dcb
Hugo Ma <huwgoma@gmail.com>
2024-06-09 22:23:34 -0400

    RB130 lesson 3

--- lesson_2/ex.rb
+++ lesson_2/ex.rb
@@ -1,11 +1,43 @@
 require 'minitest/autorun'
 
-class FirstTest < Minitest::Test
-  def test_value_equality
-    str1 = 'hello'
-    str2 = 'hello'
+class Cat
+  attr_accessor :name, :purr_factor
 
-    assert_equal(str1, str2)
-    assert_same(str1, str2)
+  def initialize(name, purr_factor)
+    @name = name
+    @purr_factor = purr_factor
   end
+
+  def miaow
+    "#{name} is miaowing."
+  end
+end
+
+class CatTest < Minitest::Test
+  def setup
+    @kitty = Cat.new('Kitty', 10)
+  end
+
+  def test_is_cat; end
+
+  def test_name
+    assert_equal('Milo', @kitty.name)
+  end
+
+  def test_miaow
+    assert_match(/ is miaowing\./, @kitty.miaow)
+  end
+
+  def test_raises_error
+    assert_raises(ArgumentError) { Cat.new }
+  end
+
+  def test_is_not_purrier
+    patch = Cat.new('Patch', 5)
+    milo = Cat.new('Milo', 3)
+  
+    refute(patch.purr_factor > milo.purr_factor)
+  end
+  # Fail, because 5 > 3 evaluates to truthy, and refute
+  # asserts that the provided value is NOT truthy.
 end
---
+++ lesson_3/ex.rb
@@ -0,0 +1,6 @@
+require 'rubocop' # Require the rubocop gem.
+
+puts $LOADED_FEATURES.grep(/rubocop\.rb/)
+#=> /home/hugo/.rbenv/versions/3.2.2/lib/ruby/gems/3.2.0/gems/rubocop-1.51.0/lib/rubocop.rb
+
+
